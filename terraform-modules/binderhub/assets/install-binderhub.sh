0f278d97d69174a0d4b84b24a3ef233991925087
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2019-03-26 10:13:18 -0400

    Add nginx-ingress

--- terraform-modules/binderhub/assets/install-binderhub.sh
+++ terraform-modules/binderhub/assets/install-binderhub.sh
@@ -3,7 +3,8 @@ while [ ! -f /var/lib/cloud/instance/boot-finished ]; do sleep 10; done
 
 cd /home/${admin_user}
 kubectl create -f pv.yaml
-sudo helm install --name kube-lego --namespace=support  stable/kube-lego -f kube-lego.yaml
+sudo helm install --name nginx-ingress --namespace=support stable/nginx-ingress -f nginx-ingress.yaml
+sudo helm install --name kube-lego --namespace=support stable/kube-lego -f kube-lego.yaml
 sudo helm repo add jupyterhub https://jupyterhub.github.io/helm-chart
 sudo helm repo update
 sudo helm install jupyterhub/binderhub --version=${binder_version} \
