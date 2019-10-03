601670cd8ba75bf98415a1a5a9fa5971be526070
Mathieu Boudreau <emb6150@gmail.com>
2019-10-03 16:47:19 -0230

    Update main.tf

--- terraform-modules/dns/cloudflare/main.tf
+++ terraform-modules/dns/cloudflare/main.tf
@@ -1,7 +1,8 @@
 provider "cloudflare" {}
 
 resource "cloudflare_record" "domain" {
-  zone_id = "${join(".", slice(split(".", var.domain), 1, length(split(".", var.domain))))}"
+  version = "<= 1.13.0"
+  domain = "${join(".", slice(split(".", var.domain), 1, length(split(".", var.domain))))}"
   name   = "${element(split(".", var.domain), 0)}"
   value  = "${var.public_ip}"
   type   = "A"
