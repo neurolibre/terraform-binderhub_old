86603acb2e621a0ab4a96d876325e9de0d3c752d
Mathieu Boudreau <emb6150@gmail.com>
2019-10-03 16:49:47 -0230

    Update main.tf

--- terraform-modules/dns/cloudflare/main.tf
+++ terraform-modules/dns/cloudflare/main.tf
@@ -1,7 +1,8 @@
-provider "cloudflare" {}
+provider "cloudflare" {
+  version = "<= 1.13.0"
+}
 
 resource "cloudflare_record" "domain" {
-  version = "<= 1.13.0"
   domain = "${join(".", slice(split(".", var.domain), 1, length(split(".", var.domain))))}"
   name   = "${element(split(".", var.domain), 0)}"
   value  = "${var.public_ip}"
