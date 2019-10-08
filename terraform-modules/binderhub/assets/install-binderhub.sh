f963a02f97ccd85683f88801ba4dab788556225a
ltetrel <37963074+ltetrel@users.noreply.github.com>
2019-10-08 16:19:17 -0400

    Update install-binderhub.sh
    Added helm installation

--- terraform-modules/binderhub/assets/install-binderhub.sh
+++ terraform-modules/binderhub/assets/install-binderhub.sh
@@ -2,6 +2,10 @@
 while [ ! -f /var/lib/cloud/instance/boot-finished ]; do sleep 10; done
 
 cd /home/${admin_user}
+curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get | bash
+kubectl --namespace kube-system create serviceaccount tiller
+helm init --service-account tiller --wait
+kubectl patch deployment tiller-deploy --namespace=kube-system --type=json --patch='[{"op": "add", "path": "/spec/template/spec/containers/0/command", "value": ["/tiller", "--listen=localhost:44134"]}]'
 kubectl create -f pv.yaml
 sudo helm install --name nginx-ingress --namespace=support stable/nginx-ingress -f nginx-ingress.yaml
 sudo helm install --name kube-lego --namespace=support stable/kube-lego -f kube-lego.yaml
