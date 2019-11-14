0f8ca34a8f1031e58e9f84ea2fd9274f28f1416e
ltetrel <37963074+ltetrel@users.noreply.github.com>
2019-11-13 22:04:01 -0500

    Update install-binderhub.sh
    https://github.com/jupyterhub/binderhub/issues/284

--- terraform-modules/binderhub/assets/install-binderhub.sh
+++ terraform-modules/binderhub/assets/install-binderhub.sh
@@ -3,8 +3,9 @@ while [ ! -f /var/lib/cloud/instance/boot-finished ]; do sleep 10; done
 
 cd /home/${admin_user}
 kubectl create -f pv.yaml
-sudo helm install --name nginx-ingress --namespace=support stable/nginx-ingress -f nginx-ingress.yaml
-sudo helm install --name kube-lego --namespace=support stable/kube-lego -f kube-lego.yaml
+kubectl apply -f binderhub-issuer.yaml
+sudo helm install stable/nginx-ingress --name quickstart
+kubectl get service quickstart
 sudo helm repo add jupyterhub https://jupyterhub.github.io/helm-chart
 sudo helm repo update
 sudo helm install jupyterhub/binderhub --version=${binder_version} \
