60abc53c3f22a8da1f791e2f154df026ee941d88
ltetrel <37963074+ltetrel@users.noreply.github.com>
2019-11-14 00:35:29 -0500

    Update install-binderhub.sh
    forgot sudo for helm..

--- terraform-modules/binderhub/assets/install-binderhub.sh
+++ terraform-modules/binderhub/assets/install-binderhub.sh
@@ -9,9 +9,9 @@ kubectl create -f pv.yaml
 # Certificate manager
 kubectl apply --validate=false -f https://raw.githubusercontent.com/jetstack/cert-manager/release-0.11/deploy/manifests/00-crds.yaml
 kubectl create namespace cert-manager
-helm repo add jetstack https://charts.jetstack.io
-helm repo update
-helm install \
+sudo helm repo add jetstack https://charts.jetstack.io
+sudo helm repo update
+sudo helm install \
   --name cert-manager \
   --namespace cert-manager \
   --version v0.11.0 \
