8414d8aca1c7c88fb5d7f4f3eee97411a48123c6
ltetrel <loic.tetrel.pro@gmail.com>
2020-02-19 13:21:54 -0500

    removing sudo execution for helm

--- terraform-modules/binderhub/assets/install-binderhub.sh
+++ terraform-modules/binderhub/assets/install-binderhub.sh
@@ -10,9 +10,9 @@ kubectl create -f pv.yaml
 sudo helm install --name nginx-ingress --namespace=support stable/nginx-ingress -f nginx-ingress.yaml
 kubectl apply -f https://raw.githubusercontent.com/jetstack/cert-manager/v0.13.1/deploy/manifests/00-crds.yaml
 kubectl create namespace cert-manager
-sudo helm repo add jetstack https://charts.jetstack.io
-sudo helm repo update
-sudo helm install \
+helm repo add jetstack https://charts.jetstack.io
+helm repo update
+helm install \
   --name cert-manager \
   --namespace cert-manager \
   --version v0.13.1 \
@@ -21,8 +21,8 @@ kubectl apply -f staging-binderhub-issuer.yaml
 kubectl get service nginx-ingress
 
 # Binderhub
-sudo helm repo add jupyterhub https://jupyterhub.github.io/helm-chart
-sudo helm repo update
-sudo helm install jupyterhub/binderhub --version=${binder_version} \
+helm repo add jupyterhub https://jupyterhub.github.io/helm-chart
+helm repo update
+helm install jupyterhub/binderhub --version=${binder_version} \
   --name=binderhub --namespace=binderhub -f config.yaml -f secrets.yaml
 sudo apt-get install git-crypt
