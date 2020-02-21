d4920d26878a90969450a484e19e09ad9af7c245
ltetrel <37963074+ltetrel@users.noreply.github.com>
2020-02-21 10:48:08 -0500

    Update variables.tf
    Adding TLS cert name as input

--- terraform-modules/binderhub/variables.tf
+++ terraform-modules/binderhub/variables.tf
@@ -5,6 +5,10 @@ variable "domain" {
   description = "Domain name"
 }
 
+variable "TLS_name" {
+  description = "TLS certificate name, same as domain but with dashes instead of points"
+}
+
 variable "TLS_email" {
   description = "Email address used to register TLS certificate"
 }
