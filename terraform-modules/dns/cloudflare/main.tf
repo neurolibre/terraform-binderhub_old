9651e53e38343c515494d61feb1a2741f7f5c964
ltetrel <37963074+ltetrel@users.noreply.github.com>
2020-02-25 13:04:06 -0500

    Update main.tf
    reusing assigned floating ip

--- terraform-modules/dns/cloudflare/main.tf
+++ terraform-modules/dns/cloudflare/main.tf
@@ -5,7 +5,6 @@ provider "cloudflare" {
 resource "cloudflare_record" "domain" {
   domain = "${join(".", slice(split(".", var.domain), 1, length(split(".", var.domain))))}"
   name   = "${element(split(".", var.domain), 0)}"
-  #value  = "${var.public_ip}"
-  value = "206.12.92.228"
+  value  = "${var.public_ip}"
   type   = "A"
 }
