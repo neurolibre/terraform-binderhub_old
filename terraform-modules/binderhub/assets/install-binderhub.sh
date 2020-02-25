4966ad3d176be5b09b126aa9803f46605ef2835e
ltetrel <37963074+ltetrel@users.noreply.github.com>
2020-02-25 17:05:43 -0500

    Update install-binderhub.sh
    I will try manually

--- terraform-modules/binderhub/assets/install-binderhub.sh
+++ terraform-modules/binderhub/assets/install-binderhub.sh
@@ -8,25 +8,25 @@ kubectl create -f pv.yaml
 
 # TLS certificate management
 # cert-manager
-kubectl apply -f https://raw.githubusercontent.com/jetstack/cert-manager/v0.11.0/deploy/manifests/00-crds.yaml
-kubectl create namespace cert-manager
-kubectl label namespace cert-manager certmanager.k8s.io/disable-validation=true
-sudo helm repo add jetstack https://charts.jetstack.io
-sudo helm repo update
-sudo helm install \
-  --name cert-manager \
-  --namespace cert-manager \
-  --version v0.11.0 \
-  jetstack/cert-manager
-kubectl create namespace binderhub
-kubectl apply -f staging-binderhub-issuer.yaml
+#kubectl apply -f https://raw.githubusercontent.com/jetstack/cert-manager/v0.13.1/deploy/manifests/00-crds.yaml
+#kubectl create namespace cert-manager
+#kubectl label namespace cert-manager certmanager.k8s.io/disable-validation=true
+#sudo helm repo add jetstack https://charts.jetstack.io
+#sudo helm repo update
+#sudo helm install \
+#  --name cert-manager \
+#  --namespace cert-manager \
+#  --version v0.13.1 \
+#  jetstack/cert-manager
+#kubectl create namespace binderhub
+#kubectl apply -f staging-binderhub-issuer.yaml
 # Binderhub proxy
-sudo helm install --name binderhub-proxy --namespace=binderhub stable/nginx-ingress -f nginx-ingress.yaml
-kubectl get services --namespace binderhub binderhub-proxy-nginx-ingress-controller
+#sudo helm install --name binderhub-proxy --namespace=binderhub stable/nginx-ingress -f nginx-ingress.yaml
+#kubectl get services --namespace binderhub binderhub-proxy-nginx-ingress-controller
 
 # Binderhub
-sudo helm repo add jupyterhub https://jupyterhub.github.io/helm-chart
-sudo helm repo update
-sudo helm install jupyterhub/binderhub --version=${binder_version} \
-  --name=binderhub --namespace=binderhub -f config.yaml -f secrets.yaml
-sudo apt-get install git-crypt
+#sudo helm repo add jupyterhub https://jupyterhub.github.io/helm-chart
+#sudo helm repo update
+#sudo helm install jupyterhub/binderhub --version=${binder_version} \
+#  --name=binderhub --namespace=binderhub -f config.yaml -f secrets.yaml
+#sudo apt-get install git-crypt
