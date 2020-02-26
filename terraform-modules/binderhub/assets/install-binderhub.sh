b1d390a355bfe847422bb6bf7e691c18ca4083e9
ltetrel <37963074+ltetrel@users.noreply.github.com>
2020-02-26 16:41:29 -0500

    Update install-binderhub.sh
    tmp adding sleep

--- terraform-modules/binderhub/assets/install-binderhub.sh
+++ terraform-modules/binderhub/assets/install-binderhub.sh
@@ -4,7 +4,7 @@ while [ ! -f /var/lib/cloud/instance/boot-finished ]; do sleep 10; done
 while [ ! -f /shared/node-initialized ]; do sleep 1; done
 while [ ! -f /shared/master-initialized ]; do sleep 1; done
 #TMP FIXME
-sleep 15m
+sleep 30m
 
 cd /home/${admin_user}
 
