469ca596437d1357e24e914676511e5b99d895f6
ltetrel <37963074+ltetrel@users.noreply.github.com>
2020-03-03 15:08:05 -0500

    Update outputs.tf

--- terraform-modules/providers/openstack/outputs.tf
+++ terraform-modules/providers/openstack/outputs.tf
@@ -5,3 +5,15 @@ output "public_ip" {
 output "admin_user" {
 	value = "${var.admin_user}"
 }
+
+output "docker_registry" {
+	value = "${var.docker_registry}"
+}
+
+output "docker_id" {
+	value = "${var.docker_id}"
+}
+
+output "docker_password" {
+	value = "${var.docker_password}"
+}
