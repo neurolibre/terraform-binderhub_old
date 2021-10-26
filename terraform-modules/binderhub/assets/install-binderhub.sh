3b39a3b6dce164fccced57689a1bb9b77b906614
Loic Tetrel <37963074+ltetrel@users.noreply.github.com>
2021-10-26 17:12:32 -0400

    Update install-binderhub.sh
    updating to helm v3

--- terraform-modules/binderhub/assets/install-binderhub.sh
+++ terraform-modules/binderhub/assets/install-binderhub.sh
@@ -13,10 +13,11 @@ kubectl create -f pv.yaml
 kubectl create namespace cert-manager
 sudo helm repo add jetstack https://charts.jetstack.io
 sudo helm repo update
-sudo helm install --name cert-manager --namespace cert-manager --version v1.0.3 jetstack/cert-manager --set installCRDs=true \
---set nodeSelector."node-role\.kubernetes\.io/master=" \
---set cainjector.nodeSelector."node-role\.kubernetes\.io/master=" \
---set webhook.nodeSelector."node-role\.kubernetes\.io/master="
+# running on master node to avoid issues with webhook not in the k8s network
+sudo helm install cert-manager --namespace cert-manager --version v1.0.3 jetstack/cert-manager --set installCRDs=true \
+  --set nodeSelector."node-role\.kubernetes\.io/master=" \
+  --set cainjector.nodeSelector."node-role\.kubernetes\.io/master=" \
+  --set webhook.nodeSelector."node-role\.kubernetes\.io/master="
 #wait until cert-manager is ready
 kubectl wait --namespace cert-manager \
   --for=condition=ready pod \
@@ -29,20 +30,26 @@ kubectl apply -f production-binderhub-issuer.yaml
 
 # Binderhub proxy
 sudo helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx/
-sudo helm install --name binderhub-proxy --namespace=binderhub ingress-nginx/ingress-nginx -f nginx-ingress.yaml
+sudo helm install binderhub-proxy ingress-nginx/ingress-nginx --namespace=binderhub -f nginx-ingress.yaml
 # wait until nginx is ready (https://kubernetes.github.io/ingress-nginx/deploy/)
 kubectl wait --namespace binderhub \
   --for=condition=ready pod \
   --selector=app.kubernetes.io/component=controller \
   --timeout=120s
-kubectl get services --namespace binderhub binderhub-proxy-nginx-ingress-controller
+kubectl get services --namespace binderhub binderhub-proxy-ingress-nginx-controller
 
 # Binderhub
+# schedule binderhub core pods just on master
+# https://alan-turing-institute.github.io/hub23-deploy/advanced/optimising-jupyterhub.html#labelling-nodes-for-core-purpose
+kubectl label nodes neurolibre-master hub.jupyter.org/node-purpose=core
 sudo helm repo add jupyterhub https://jupyterhub.github.io/helm-chart
 sudo helm repo update
-sudo helm install jupyterhub/binderhub --version=${binder_version} \
-  --name=binderhub --namespace=binderhub -f config.yaml -f secrets.yaml
-sudo apt-get install git-crypt
+sudo helm install binderhub jupyterhub/binderhub --version=${binder_version} \
+  --namespace=binderhub -f config.yaml -f secrets.yaml
+kubectl wait --namespace binderhub \
+  --for=condition=ready pod \
+  --selector=release=binderhub \
+  --timeout=120s
 # Grafana and prometheus
 # https://github.com/pangeo-data/pangeo-binder#binder-monitoring
 sudo helm repo add grafana https://grafana.github.io/helm-charts
